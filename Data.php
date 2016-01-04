7089acf1680817fc88eda8221b7f78612a86eba2
Godfrey Mlowe <mlowegodfrey@gmail.com>
2016-01-04 10:48:27 +0300

    $ git init

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysql'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = '123123123'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+?>
